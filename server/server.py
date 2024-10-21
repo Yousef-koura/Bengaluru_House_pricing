555dd635e30ec51172bb4f155896dc17a926a200
Yousef-Koura <user.yousefahmed.ae20@gmail.com>
2024-10-21 20:58:31 +0300

    modified

--- server/server.py
+++ server/server.py
@@ -3,6 +3,7 @@ from flask import Flask,request,jsonify
 import pickle
 import util
 
+
 app = Flask(__name__)
 
 
@@ -15,6 +16,24 @@ def get_location_names():
 
     return response
 
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bedrooms = int(request.form['bedrooms'])
+    bath = int(request.form['bath'])
+    balcony = int(request.form['balcony'])
+    area_type = int(request.form['area_type'])
+    
+    response = jsonify({
+        'estimated_price':util.predict_price(location, area_type, bath, balcony, bedrooms, total_sqft)
+    })
+
+    response.headers.add('Access-Control-Allow-Origin','*')
+
+    return response 
+
 if __name__ == "__main__":
     print("Starting Python Flask Server for Home Prediction")
+    util.load_saved_artifacts()
     app.run()
