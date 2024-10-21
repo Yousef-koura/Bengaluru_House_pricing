e4dc7733935371511d671ed7fb2a6b7c8809a19f
Yousef-Koura <user.yousefahmed.ae20@gmail.com>
2024-10-21 19:18:01 +0300

    modified

--- server/server.py
+++ server/server.py
@@ -1,6 +1,20 @@
 
-from fastapi import FastAPI
+from flask import Flask,request,jsonify
 import pickle
+import util
 
-app = FastAPI()
+app = Flask(__name__)
 
+
+@app.route("/get_location_names")
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin','*')
+
+    return response
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server for Home Prediction")
+    app.run()
--- server/util.py
+++ server/util.py
@@ -1,5 +1,7 @@
 import json
 import pickle
+import pandas as pd
+import numpy as np 
 
 __locations = None
 __data_columns = None
@@ -16,14 +18,36 @@ def load_saved_artifacts():
     global __model
 
     # Load column names from JSON file
-    with open("./server/Artifacts/columns.json", 'r') as f:
-        __data_columns = json.load(f)['data_columns']  # Ensure 'data_columns' is correct
-        __locations = __data_columns[4:]  # Assuming the first 3 columns are non-location-related
-
+    __data_columns = json.load(open("./server/Artifacts/columns.json", 'r'))['data_columns']  
+    # Slice locations from the data columns 
+    __locations = __data_columns[4:]  
     # Load the trained model from pickle file
-    with open("./server/Artifacts/banglore_home_price_model.pickle", "rb") as f:
-        __model = pickle.load(f)
+    __model = pickle.load(open("./server/Artifacts/Housing_model.pkl", "rb"))
+
+
+# Define the prediction function
+def predict_price(location, area_type, bath, balcony, bedrooms, total_sqft):
+    # Get the index of the location in the data columns (if it exists)
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except ValueError:
+        loc_index = -1  # If location is not found in columns, use -1 (will remain 0)
+
+# Prepare the input data as an array with zeros for all columns
+    x = np.zeros(len(__data_columns))  # Ensure the input array has the same length as data_columns
+    
+    # Set the appropriate values for the input columns
+    x[0] = area_type  # Area Type (label encoded)
+    x[1] = bath       # Number of bathrooms
+    x[2] = balcony    # Number of balconies
+    x[3] = bedrooms   # Number of bedrooms
+    x[-2] = total_sqft  # Total square feet (should be the last column)
+    if loc_index >= False:
+        x[loc_index] = True
+
+    return round(__model.predict([x])[0],2)
 
 if __name__ == "__main__":
     load_saved_artifacts()
-    print(get_location_names())
+    predicted_price = predict_price('yelachenahalli', 1, 3, 2, 3, 1500)  # Use 0 for area_type as an example
+    print(f"The predicted price is: {predicted_price}")
